8c88cd5009bd903666c9988353a77d73f2ba6219
Alexander Kabui <alexanderkabua@gmail.com>
2021-08-13 00:56:10 +0300

    - initial python setup

--- init2.el
+++ init2.el
@@ -1,5 +1,3 @@
-;;playground for emacs 2
-
 
 (require 'package)
 
@@ -8,7 +6,6 @@
 
 ;;initializes the package infrastructure
 
-;;magit status trial
 (package-initialize)
 
 
@@ -23,6 +20,10 @@
     solarized-theme
     cyberpunk-theme
     dracula-theme
+    magit
+    elpy
+    flycheck    ;;replace flymake
+    py-autopep8  ;;autopep on save
     )
   )
 
@@ -39,15 +40,44 @@
 ;;(load-theme 'monokai-pro-classic  t)
 
 (load-theme 'cyberpunk t)
-
 ;;(load-theme 'dracula t)
+
+
+
+;;;;;python-setup
+;;Enable elpy
+
+(elpy-enable)
+
+
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+
+
+;; Enable autopep8
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(setq py-autopep8-options '("--max-line-length=1"))
+
+
+
+
+
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dracula-theme solarized-theme monokai-pro-theme material-theme cyberpunk-theme better-defaults)))
+   '(dracula-theme solarized-theme monokai-pro-theme material-theme cyberpunk-theme better-defaults))
+ '(send-mail-function 'mailclient-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
